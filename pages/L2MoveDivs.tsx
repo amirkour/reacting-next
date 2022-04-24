193c86eeebf9383766334c36824fd505fdaed173
Amir Kouretchian <amir.kouretchian@ridwell.com>
2022-04-24 14:01:23 -0700

    wip - media query not cooperating

--- pages/L2MoveDivs.tsx
+++ pages/L2MoveDivs.tsx
@@ -2,9 +2,29 @@ import React from "react";
 
 const L2MoveDivs = () => (
   <>
-    <div>L2MoveDivs</div>
+    <h1 style={{ textAlign: "center" }}>Learn To Move Divs!?</h1>
+    <div className="container">
+      <div className="box">box 1</div>
+      <div className="box">box 2</div>
+    </div>
     <style jsx>{`
-      @import "theme";
+      .container {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        justify-content: space-evenly;
+        border: 2px solid blue;
+        @media (min-width: 768px) {
+          flex-direction: column;
+          align-items: center;
+        }
+      }
+
+      .box {
+        width: 370px;
+        height: 370px;
+        border: 1px solid red;
+      }
     `}</style>
   </>
 );
