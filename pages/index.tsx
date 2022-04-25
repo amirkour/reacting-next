f1ee6602f38c2168781569e5aa5bb74609bb38ab
Amir Kouretchian <amir.kouretchian@gmail.com>
2022-04-24 22:09:40 -0700

    starting the move to tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World!?</h1>
+      <h1 className="text-center">Hello World!?</h1>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      xs: "375px",
+      sm: "576px",
+      ...defaultTheme.screens,
+    },
+    //   // md: "768px",
+    //   // lg: "1024px",
+    //   // xl: "1280px",
+    //   // "2xl": "1536px",
+    // },
     extend: {},
   },
   plugins: [],
-}
+};
